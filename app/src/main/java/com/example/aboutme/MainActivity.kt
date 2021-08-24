ee1ec499410d72e0030dc5c4f33303e4c4870de8
Sergeev Sergey <Sergeev_sergey99@mail.ru>
2021-08-25 02:48:27 +0300

    about me final

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -9,26 +9,41 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
+
 class MainActivity : AppCompatActivity() {
+
+    private  val myName = MyName("Serg", "Archenemy")
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
             addNicname(it)
         }
     }
 
-    private fun addNicname(view: View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+    private fun addNicname(view: View) {
+        //val editText = findViewById<EditText>(R.id.nickname_edit)
+        //val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
-
+        binding.apply {
+            nicknameText.text = nicknameEdit.text
+            //invalidateAll()
+            //onResume()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken,0)
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
---
+++ app/src/main/java/com/example/aboutme/MyName.kt
@@ -0,0 +1,6 @@
+package com.example.aboutme
+
+data class MyName(
+    var name: String = "",
+    var nickname: String = ""
+)
